ae3e2a60633409bf3b6e533ee47a57ae1049e63a
raul4247 <raulfmansur@hotmail.com>
2019-04-16 09:09:58 -0300

    Updates

---
+++ precache-manifest.fec2de60f8f6fe7b326b78e61949883c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-heist-frontend/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fb42afc3510c459a97e1",
+    "url": "/color-heist-frontend/static/js/runtime~main.3f4f8a9c.js"
+  },
+  {
+    "revision": "01b8215d8d7afdb7aa6a",
+    "url": "/color-heist-frontend/static/js/main.b7b88eb3.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/color-heist-frontend/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "01b8215d8d7afdb7aa6a",
+    "url": "/color-heist-frontend/static/css/main.4989568f.chunk.css"
+  },
+  {
+    "revision": "514d8390cb5c4e859eba698995b481ac",
+    "url": "/color-heist-frontend/index.html"
+  }
+];
