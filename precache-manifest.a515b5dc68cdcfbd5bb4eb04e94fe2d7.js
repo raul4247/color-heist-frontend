63f1e13be613bf0330b09a13057f79b046ad8300
raul4247 <raulfmansur@hotmail.com>
2019-04-16 10:07:39 -0300

    Updates

---
+++ precache-manifest.a515b5dc68cdcfbd5bb4eb04e94fe2d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb42afc3510c459a97e1",
+    "url": "/color-heist-frontend/static/js/runtime~main.3f4f8a9c.js"
+  },
+  {
+    "revision": "03b838df9479fb0d0758",
+    "url": "/color-heist-frontend/static/js/main.c2a21564.chunk.js"
+  },
+  {
+    "revision": "655c12d741d1db6c4cd1",
+    "url": "/color-heist-frontend/static/js/2.745c3471.chunk.js"
+  },
+  {
+    "revision": "03b838df9479fb0d0758",
+    "url": "/color-heist-frontend/static/css/main.bf72948d.chunk.css"
+  },
+  {
+    "revision": "89c27c9d39a66c383fc133ac70cbccf4",
+    "url": "/color-heist-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/color-heist-frontend/precache-manifest.fec2de60f8f6fe7b326b78e61949883c.js"
+  "/color-heist-frontend/precache-manifest.a515b5dc68cdcfbd5bb4eb04e94fe2d7.js"
 );
 
 workbox.clientsClaim();
